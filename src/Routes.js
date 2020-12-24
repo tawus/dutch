47680605cdc8b11fbd04967f669dc84b92c64a9d
Taha Siddiqi <taha.siddiqi@xeric.net>
2020-12-24 23:28:54 +0530

    Add Page titles

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Groups from './features/groups/Groups';
 import Contacts from './features/contacts/Contacts';
 import AddContact from './features/contacts/AddContact';
 import AddGroup from './features/groups/AddGroup';
 import GroupDetails from './features/groups/GroupDetails';
 import ContactDetails from './features/contacts/ContactDetails';
+import Page from './components/Page';
 
 const Routes = () => (
     <Switch>
-        <Route path="/contacts/new" component={AddContact} exact />
-        <Route path="/contacts/:id" component={ContactDetails} exact />
-        <Route path="/contacts" component={Contacts} exact />
-        <Route path="/groups/new" component={AddGroup} exact />
-        <Route path="/groups/:id" component={GroupDetails} exact />
-        <Route path="/" component={Groups} />
+        <Page
+            path="/contacts/new"
+            component={AddContact}
+            exact
+            title={title('Create New Contact')}
+        />
+        <Page
+            path="/contacts/:id"
+            component={ContactDetails}
+            exact
+            title={title('Contact Details')}
+        />
+        <Page
+            path="/contacts"
+            component={Contacts}
+            exact
+            title={title('Contacts')}
+        />
+        <Page
+            path="/groups/new"
+            component={AddGroup}
+            exact
+            title={title('Create New Group')}
+        />
+        <Page
+            path="/groups/:id"
+            component={GroupDetails}
+            exact
+            title={title('Group Details')}
+        />
+        <Page path="/" component={Groups} title={title('Home')} />
     </Switch>
 );
 
 export default Routes;
+
+const title = text => `DUTCH: ${text}`;
